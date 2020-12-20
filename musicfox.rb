1d13029eabb27905461f0ca602e307cc312ecdef
anhoder <alan1766447919@gmail.com>
2020-12-20 19:02:37 +0800

    Update musicfox.rb

--- musicfox.rb
+++ musicfox.rb
@@ -3,8 +3,8 @@
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Musicfox < Formula
   desc "musicfox是一款使用Dart编写的网易云音乐命令行程序。"
-  homepage "https://github.com/AlanAlbert/musicfox"
-  url "https://github.com/AlanAlbert/musicfox/releases/download/1.2.0/musicfox.mac"
+  homepage "https://github.com/anhoder/musicfox"
+  url "https://github.com/anhoder/musicfox/releases/download/1.2.0/musicfox.mac"
   version "1.2.0"
   sha256 "dcf77ff159357c724bb52a407df7edb13faec9e9102388bb8f0230d5d43315d5"
 
@@ -13,7 +13,7 @@ class Musicfox < Formula
 
   def install
     system 'mv musicfox.mac musicfox'
-    system 'echo', '-e', "\033[1;33m给个star✨支持一下吧~\033[0m ==> \033[4;36mhttps://github.com/AlanAlbert/musicfox\033[0m"
+    system 'echo', '-e', "\033[1;33m给个star✨支持一下吧~\033[0m ==> \033[4;36mhttps://github.com/anhoder/musicfox\033[0m"
     bin.install 'musicfox'
   end
 
